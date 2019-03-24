0a1449626dcb82cb6432b5807c2ab91ecafaeff5
Natalia Ortiz <natorgom@gmail.com>
2019-03-24 19:05:31 +0100

    add comments

--- droneFlightPlanner/src/app/dashboard/dashboard.component.ts
+++ droneFlightPlanner/src/app/dashboard/dashboard.component.ts
@@ -15,82 +15,80 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 })
 export class DashboardComponent implements OnInit {
 
-  lastPosition: Point;
-  currentPosition: Point;
-  maps: Map[];
-  currentMapName: string;
-  currentMapLines: Line[];
-  buttonDisabled: boolean;
-
+  lastPosition: Point; // Last position selected in the canvas
+  currentPosition: Point; // Current position selected in the canvas
+  maps: Map[]; // List of maps created
+  currentMapName: string; // Name of the current map
+  currentMapLines: Line[]; // List of lines for a specific map
+  buttonDisabled: boolean; // Flag to enable/disable the save button
+  canvas: any; // Canvas
+  canvasContext: any; // Context of the canvas
 
   constructor(@Inject(DOCUMENT) document) {
   }
 
   ngOnInit() {
-    const canvas: any = document.getElementById('imgCanvas');
-    canvas.width  = 600;
-    canvas.height = 600;
+
+    // Initialise canvas
+    this.canvas = document.getElementById('imgCanvas');
+    this.canvasContext = this.canvas.getContext('2d');
+    this.canvas.width  = 600;
+    this.canvas.height = 600;
     this.currentMapName = '';
     this.maps = [];
     this.currentMapLines = [];
+
+    // Initially disable save button
     this.buttonDisabled = true;
   }
 
-  addLine(canvas, currentPosition, lastPosition) {
+  // Add a line that connects two points in the canvas
+  addLine(currentPosition, lastPosition) {
     if (lastPosition !== undefined) {
-      if (canvas.getContext) {
-        const context = canvas.getContext('2d');
-        context.beginPath();
-        context.moveTo(lastPosition.x, lastPosition.y);
-        context.lineTo(currentPosition.x, currentPosition.y);
-        context.lineWidth = 3;
-        context.strokeStyle = 'blue';
-        context.stroke();
-        this.currentMapLines.push({
-          initialPoint: lastPosition,
-          finalPoint: currentPosition
-        });
-      }
+      this.canvasContext.beginPath();
+      this.canvasContext.moveTo(lastPosition.x, lastPosition.y);
+      this.canvasContext.lineTo(currentPosition.x, currentPosition.y);
+      this.canvasContext.lineWidth = 3;
+      this.canvasContext.strokeStyle = 'blue';
+      this.canvasContext.stroke();
+      this.currentMapLines.push({
+        initialPoint: lastPosition,
+        finalPoint: currentPosition
+      });
     }
   }
 
-  getMousePos(canvas, evt) {
-    const rect = canvas.getBoundingClientRect();
+  // Get mouse position
+  getMousePos(evt) {
+    const rect = this.canvas.getBoundingClientRect();
     return {
-      x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
-      y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+      x: (evt.clientX - rect.left) / (rect.right - rect.left) * this.canvas.width,
+      y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * this.canvas.height
     };
   }
 
+  // Add a point in the canvas
   addDot(e) {
-    const canvas: any = document.getElementById('imgCanvas');
-    this.currentPosition = this.getMousePos(canvas, e);
-
-    if (canvas.getContext) {
-      const context = canvas.getContext('2d');
-      this.drawPoint(context, this.currentPosition);
-      this.addLine(canvas, this.currentPosition, this.lastPosition);
+    this.currentPosition = this.getMousePos(e);
+    if (this.canvas.getContext) {
+      this.drawPoint(this.currentPosition);
+      this.addLine(this.currentPosition, this.lastPosition);
       this.lastPosition = this.currentPosition;
     }
   }
 
+  // Clear the canvas
   clearCanvas() {
     this.currentMapName = '';
     this.currentMapLines = [];
-    const canvas: any = document.getElementById('imgCanvas');
-    const context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.lastPosition = undefined;
     this.currentPosition = undefined;
     this.buttonDisabled = true;
   }
 
-  clearInput() {
-    const input: any = document.getElementById('imgCanvas');
-  }
-
+  // Enable/disable save button based on input value existence
   onKey() {
-    console.log('currentMapName', this.currentMapName)
     if (this.currentMapName) {
       this.buttonDisabled = false;
     } else {
@@ -98,6 +96,7 @@ export class DashboardComponent implements OnInit {
     }
   }
 
+  // Save the map when the save button is clicked
   saveMap() {
     this.maps.push({
       name: this.currentMapName,
@@ -107,30 +106,30 @@ export class DashboardComponent implements OnInit {
     this.clearCanvas();
   }
 
+  // Upload an existent map on the canvas
   uploadMap(i: number) {
     this.clearCanvas();
     const selectedMap = this.maps[i];
     this.currentMapName = selectedMap.name;
     this.buttonDisabled = false;
-    const canvas: any = document.getElementById('imgCanvas');
-    const context = canvas.getContext('2d');
     selectedMap.lines.forEach(line => {
       const initialPoint = line.initialPoint;
       const finalPoint = line.finalPoint;
-      this.drawPoint(context, initialPoint);
-      this.drawPoint(context, finalPoint);
-      this.addLine(canvas, initialPoint, finalPoint);
+      this.drawPoint(initialPoint);
+      this.drawPoint(finalPoint);
+      this.addLine(initialPoint, finalPoint);
     });
   }
 
-  drawPoint(context, position) {
-    context.beginPath();
-    context.moveTo(position.x, position.y);
-    context.arc(position.x, position.y, 5, 0, 2 * Math.PI);
-    context.lineWidth = 1;
-    context.fillStyle = 'blue';
-    context.fill();
-    context.stroke();
+  // Draw a point on the canvas
+  drawPoint(position) {
+    this.canvasContext.beginPath();
+    this.canvasContext.moveTo(position.x, position.y);
+    this.canvasContext.arc(position.x, position.y, 5, 0, 2 * Math.PI);
+    this.canvasContext.lineWidth = 1;
+    this.canvasContext.fillStyle = 'blue';
+    this.canvasContext.fill();
+    this.canvasContext.stroke();
   }
 
 }
